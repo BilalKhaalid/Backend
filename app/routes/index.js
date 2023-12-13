8cea40895791bccccb86b39f441df84244c66af5
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-13 16:28:32 +0500

    created routes for create read delete

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const usersModel = require("./users");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+// ! Create
+
+router.get("/create", async (req, res) => {
+  const createdUser = await usersModel.create({
+    username: "Bilal Khalid",
+    name: "Bilal2",
+    age: 21,
+  });
+  res.send(createdUser);
+});
+
+// ! Read
+router.get("/all", async (req, res) => {
+  const allUsers = await usersModel.find();
+  res.send(allUsers);
+});
+
+// ! Delete
+router.get("/delete", async (req, res) => {
+  const deletedUser = await usersModel.findOneAndDelete({
+    name: "Bilal",
+  });
+  res.send(deletedUser);
 });
 
 module.exports = router;
