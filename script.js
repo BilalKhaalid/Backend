cfe8940ffb28ad2a0c5a3508636aa5e1c718d825
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-13 11:14:12 +0500

    express js basics

---
+++ script.js
@@ -0,0 +1,26 @@
+// ! How to use Express
+
+const express = require("express");
+const app = express();
+const port = 5002;
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/profile/:username", (a, b, c) => {
+  b.send(`Hello i am ${a.params.username}`);
+});
+
+app.use(function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render("error", { error: err });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
