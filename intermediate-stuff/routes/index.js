8c0981cc8c8398f8518feb0c95d6c223adc35162
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-14 16:38:27 +0500

    How to search by categories?

--- intermediate-stuff/routes/index.js
+++ intermediate-stuff/routes/index.js
@@ -23,7 +23,7 @@ router.get("/create", async (req, res) => {
     username: "Bilal Khalid",
     nickname: "Khalid",
     description: "i am bilal khalid ",
-    categories: ["js", "node", "react"],
+    categories: ["science", "psychology", "js"],
     dateCreated: new Date(),
   });
   res.send(userData);
@@ -35,20 +35,33 @@ router.get("/read", async (req, res) => {
   res.send(allDocs);
 });
 
+// router.get("/find", async (req, res) => {
+//   // ? How to perform case-Insensitive search in Mongoose?
+
+//   // * There's a problem with this approach too that if any document has the same search pattern it'll return all the documents containing search pattern
+//   // const regex = new RegExp("bilal", "i");
+
+//   // ! So we'll use this instead this match the exact word not anything containing it
+//   const regex = new RegExp("^bilAL$", "i");
+//   const caseSearch = await userModel.find({
+//     // ! This will result in an empty array so we will use regex for case-Insensitive searches
+//     // username: "bilal",
+//     username: regex,
+//   });
+//   res.send(caseSearch);
+// });
+
+// ? How to find by categories?
 router.get("/find", async (req, res) => {
-  // ? How to perform case-Insensitive search in Mongoose?
-
-  // * There's a problem with this approach too that if any document has the same search pattern it'll return all the documents containing search pattern
-  // const regex = new RegExp("bilal", "i");
-
-  // ! So we'll use this instead this match the exact word not anything containing it
-  const regex = new RegExp("^bilAL$", "i");
-  const caseSearch = await userModel.find({
-    // ! This will result in an empty array so we will use regex for case-Insensitive searches
-    // username: "bilal",
-    username: regex,
+  const findByCategory = await userModel.find({
+    categories: {
+      // ! This will search all the users having a value "js" in there categories field
+      // * $all accepts an array for multiple values searching
+      // $all: "js",
+      $all: ["js", "science", "psychology"],
+    },
   });
-  res.send(caseSearch);
+  res.send(findByCategory);
 });
 
 router.get("/delete", async (req, res) => {
