ad66a28a1e0fe4908e09f78d3cd8f8c7b952e080
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-26 23:01:50 +0500

    created a route to create a user

--- mongodb/models/db.js
+++ mongodb/models/db.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 const connectToDatabase = async () => {
   try {
     //! By writing this line in code it creates a database with the name(learning) you have given in mongodb
-    await mongoose.connect("mongodb://localhost:27017/learning");
+    await mongoose.connect("mongodb://localhost:27017/learning", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
--- mongodb/routes/index.js
+++ mongodb/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+// ! importing user model
+const User = require("../models/User");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+// ! Create User route
+router.get("/create", async (req, res) => {
+  const createdUser = await User.create({
+    username: "Bilal",
+    email: "Bilal@gmail.com",
+    password: "1245",
+    age: 22,
+  });
+  res.send(createdUser);
 });
 
 module.exports = router;
