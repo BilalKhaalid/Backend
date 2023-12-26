d85d1dae8d34e7d80e2e4859804a7572515be5f0
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-26 23:02:27 +0500

    connectToDatabase function called

--- mongodb/app.js
+++ mongodb/app.js
@@ -9,7 +9,6 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
-connectToDatabase();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -24,6 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
+connectToDatabase();
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
