2029da3757589823a6a35ee0838171c0ba549b5a
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-27 16:22:49 +0500

    added logic for connecting to database

---
+++ mongodb/intermediate/models/db.js
@@ -0,0 +1,17 @@
+// db.js
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+  try {
+    //! By writing this line in code it creates a database with the name(learning) you have given in mongodb
+    await mongoose.connect("mongodb://localhost:27017/intermediate_mongodb", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
+
+module.exports = { connectToDatabase };
