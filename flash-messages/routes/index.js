930d7dfa206f2ccee1896b24f81291201abceb40
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-27 15:57:17 +0500

    routes defined for using flash-messages

--- flash-messages/routes/index.js
+++ flash-messages/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get("/flash", function (req, res) {
+  // Set a flash message by passing the key, followed by the value, to req.flash().
+  req.flash("info", "Flash is back!");
+  res.redirect("/");
+});
+
+router.get("/", function (req, res) {
+  // Get an array of flash messages by passing the key to req.flash()
+  res.render("index", { messages: req.flash("info") });
 });
 
 module.exports = router;
