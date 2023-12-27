35bed4d06ee7fa2f22e467edb6e97c1636ceaa61
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-27 11:43:32 +0500

    required express-session and used it in middleware

--- mongodb/app.js
+++ mongodb/app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const { connectToDatabase } = require("./models/db");
+const session = require("express-session");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -19,6 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "hello_there",
+  })
+);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
